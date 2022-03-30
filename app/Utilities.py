4d2ca1dc0d5a57b05b89a81738d16866c67c86c5
dp852 <89594445+dp852@users.noreply.github.com>
2022-03-30 17:21:26 -0400

    Changes (#4)
    * Changes
    
    * Class

---
+++ app/Utilities.py
@@ -0,0 +1,21 @@
+from unicodedata import name
+
+
+def to_usd(my_price):
+    """
+    This is a doc string. It tells us what this function is about. 
+    What its responsibilities are.
+    What the parameters are about. 
+    What datatypes the parameters are. 
+    What this function will return/.
+    Example of invoking the function.
+
+    Invoke like this: to_usd(9.9999)
+    """
+    return '${:,.2f}'.format(my_price)
+
+if __name__ == "__main__":  
+    price = input("Please choose a price like 4.9999")
+    print(to_usd(float(price)))
+
+
